8da487737f6b0db67c653154dfc42cec32a8b983
Markus Werle <daixtrose@users.noreply.github.com>
2022-11-17 17:58:56 +0100

    complete rewrite of script

--- twitter-backup.sh
+++ twitter-backup.sh
@@ -3,29 +3,45 @@
 # works only when gh is installed and `gh auth login` was performed 
 # sudo apt install gh
 
-SOURCE=https://api.github.com/orgs/twitter/repos
+SOURCE_ORG=twitter
 TARGET_ORG=twitter-backup
 SLEEP_TIME_IN_SECONDS=30
 
-# echo "==> Fetching list of repositories"
+# TODO: result from this command are broken
+# gh repo list ${TARGET_ORG} --fork --json name -q '.[] | [.name][0]'
 
-# curl ${SOURCE} \
-#     | jq '.[] | [.url][0] ' \
-#     | sed -e's,"https://api.github.com/repos/,,' -e's,",,' > twitter_repos.txt
 
-# gh api orgs/twitter/repos --paginate \ 
-#     | jq '.[] | [.url][0] ' \
-#     | sed -e's,"https://api.github.com/repos/,,' -e's,",,' 2>&1 | tee twitter_repos.txt
-
-echo "==> starting forking"
+list_repos() {
+    gh api orgs/$1/repos --paginate \
+    | jq '.[] | [.url][0] ' \
+    | sed -e's,"https://api.github.com/repos/,,' -e's,",,' -e"s,$1/,," \
+    | sort \
+    | grep -v ".github"
+    
+} 
 
 wait_some_time() {
     echo "sleeping for ${SLEEP_TIME_IN_SECONDS} seconds in order to avoid getting killed by GitHub API restrictions ..."
     sleep ${SLEEP_TIME_IN_SECONDS}
 }
 
-cat twitter_repos.txt | while read repo; do
-    echo "forking ${repo}"
-    gh repo fork ${repo} --clone --org ${TARGET_ORG} && wait_some_time
+echo "==> Checking for repositories that are already forked"
+
+forked_repos=$(list_repos ${TARGET_ORG})
+echo ${forked_repos}
+
+echo
+echo "==> Fetching list of repositories"
+
+source_repos=$(list_repos ${SOURCE_ORG})
+echo ${source_repos}
+
+echo
+echo "==> starting forking"
+missing_repos=$(echo ${source_repos[@]} ${forked_repos[@]} | sed 's/ /\n/g' | sort | uniq -u)
+
+for repo in ${missing_repos}; do 
+    echo "--> forking ${repo}"
+    gh repo fork ${SOURCE_ORG}/${repo} --clone --org ${TARGET_ORG} && wait_some_time
     rm -rf $(basename ${repo})    
 done
