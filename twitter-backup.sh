73a356783531b5d4e51aee0cfbde257bb7b846c5
Markus Werle <daixtrose@users.noreply.github.com>
2022-11-17 16:26:37 +0100

    refinements

--- twitter-backup.sh
+++ twitter-backup.sh
@@ -5,7 +5,7 @@
 
 SOURCE=https://api.github.com/orgs/twitter/repos
 TARGET_ORG=twitter-backup
-SLEEP_TIME_IN_SECONDS=50
+SLEEP_TIME_IN_SECONDS=30
 
 # echo "==> Fetching list of repositories"
 
@@ -19,10 +19,13 @@ SLEEP_TIME_IN_SECONDS=50
 
 echo "==> starting forking"
 
-cat twitter_repos.txt | while read repo; do
-    echo "forking ${repo}"
-    gh repo fork ${repo} --clone --org ${TARGET_ORG}
-    rm -rf $(basename ${repo})    
+wait_some_time() {
     echo "sleeping for ${SLEEP_TIME_IN_SECONDS} seconds in order to avoid getting killed by GitHub API restrictions ..."
     sleep ${SLEEP_TIME_IN_SECONDS}
+}
+
+cat twitter_repos.txt | while read repo; do
+    echo "forking ${repo}"
+    gh repo fork ${repo} --clone --org ${TARGET_ORG} && wait_some_time
+    rm -rf $(basename ${repo})    
 done
